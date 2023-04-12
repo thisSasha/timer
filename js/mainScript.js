e8ad7a47884d86520ab73e3e489870f103622bd0
thisSasha <trhfjfn@gmail.com>
2023-04-12 22:09:13 +0300

    1

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,33 +1,33 @@
 document.querySelector('button').onclick = function () {
-    let intervalFun = function(seconds) {
-        let time = document.querySelector('#timeInp').value;
-        let timeMinutes = time - 1;
-        let interval = false;
-        let timeSeconds = 59;
-        let timerInterval = setInterval(() => {
-            interval = true;
-            timeSeconds--;
-            if (timeSeconds <= 0) {
-                timeSeconds = seconds;
-                timeMinutes--;
-            };
-            if (timeMinutes < 0) {
-                clearInterval(timerInterval);
-                interval = false;
-                new Audio('../timer.mp3').play();
-                timeMinutes = 0;
-                timeSeconds = 0;
-            };
-            document.querySelector('#minutes').innerHTML = timeMinutes;
-            document.querySelector('#seconds').innerHTML = timeSeconds;
-        }, 1000);  
-    };
-    if (document.querySelector('#timer').value>0) {
+    let time = document.querySelector('#timeInp').value;
+    let timeMinutes = time - 1;
+    let interval = false;
+    let timeSeconds = 20;
+    let timerInterval = setInterval(() => {
+        interval = true;
+        timeSeconds--;
+        if (timeSeconds <= 0) {
+            timeSeconds = 20;
+            timeMinutes--;
+        };
+        if (timeMinutes < 0) {
+            clearInterval(timerInterval);
+            interval = false;
+            new Audio('../timer.mp3').play();
+            timeMinutes = 0;
+            timeSeconds = 0;
+        };
+        document.querySelector('#minutes').innerHTML = timeMinutes;
+        document.querySelector('#seconds').innerHTML = timeSeconds;
+    }, 1000);
+    if (document.querySelector('#timer').value > 0) {
         let tOut = setTimeout(() => {
-            new Audio('chill.mp3').play();
-            setTimeout(() => {
+            if (interval) {
                 new Audio('chill.mp3').play();
+            };
+            setTimeout(() => {
                 if (interval) {
+                    new Audio('chill.mp3').play();
                     setTimeout(tOut);
                 };
             }, document.querySelector('#timer').value * 60000);
